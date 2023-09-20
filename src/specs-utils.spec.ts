80cb7ad09998cb74a3626b7ac23288252af0f767
Chris Neale <c.neale@zengenti.com>
2023-09-20 10:10:33 +0100

    users: added optOutOfNotifications property to a user that will be available in 16.0

--- src/specs-utils.spec.ts
+++ src/specs-utils.spec.ts
@@ -5,7 +5,8 @@ const defaultRootUrl = 'http://my-website.com/';
 export const defaultUsers = [{
     id: 'IIIIII1',
     username: 'UUUUUU1',
-    email: 'EEEEEE1@test.com'
+    email: 'EEEEEE1@test.com',
+    optOutOfNotifications: true
 },
 {
     id: 'IIIIII2',
