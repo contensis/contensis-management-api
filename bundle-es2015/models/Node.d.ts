0da9e1f2e9502eb360414a5b6ed4cbd4daf27ef9
Neil Flatley <n.flatley@zengenti.com>
2024-03-01 10:30:51 +0000

    chore: build assets

--- bundle-es2015/models/Node.d.ts
+++ bundle-es2015/models/Node.d.ts
@@ -1,3 +1,10 @@
+import { VersionInfo } from 'contensis-core-api';
+declare type Optional<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>> & Pick<Partial<T>, K>;
+declare type Proxy = any | null;
+declare type Renderer = any | null;
+/**
+ * A Node as it is returned from the Management API
+ */
 export interface Node {
     id: string;
     parentId?: string;
@@ -8,9 +15,28 @@ export interface Node {
     slug: {
         [key: string]: string;
     };
+    path: {
+        [key: string]: string;
+    };
     entryId?: string;
+    isCanonical: boolean;
     restrictedToLanguages: string[];
     childCount: number;
-    isCanonical: boolean;
+    renderer: Renderer;
+    proxy: Proxy;
     includeInMenu: boolean;
+    version: Partial<VersionInfo>;
 }
+/**
+ * A Node-like interface containing required properties to create a new Node
+ * and all other properties are optional
+ */
+export interface ICreateNode extends Optional<Node, 'childCount' | 'id' | 'includeInMenu' | 'isCanonical' | 'path' | 'proxy' | 'renderer' | 'restrictedToLanguages' | 'version'> {
+}
+/**
+* A Node-like interface containing required properties to update an
+* existing Node and all other properties are optional
+*/
+export interface IUpdateNode extends Optional<Node, 'childCount' | 'path' | 'proxy' | 'renderer'> {
+}
+export {};
