6fb659311bf8fb89b16292d8b4e746d9482f7dc4
Neil Flatley <n.flatley@zengenti.com>
2024-03-01 10:29:28 +0000

    fix: update management Node model, add specific interfaces derived from Node for create and update node method arg

--- src/models/INodeOperations.ts
+++ src/models/INodeOperations.ts
@@ -1,4 +1,4 @@
-import { Node } from './Node';
+import { ICreateNode, IUpdateNode, Node } from './Node';
 import { NodeGetChildrenOptions } from './NodeGetChildrenOptions';
 
 export interface INodeOperations {
@@ -6,8 +6,8 @@ export interface INodeOperations {
     get(id: string): Promise<Node>;
     getByEntryId(entryId: string): Promise<Node[]>;
     getChildren(idOrOptions: string | NodeGetChildrenOptions): Promise<Node[]>;
-    create(node: Node): Promise<Node>;
-    update(node: Node): Promise<Node>;
+    create(node: ICreateNode): Promise<Node>;
+    update(node: IUpdateNode): Promise<Node>;
     delete(id: string): Promise<void>;
     setChildrenOrder(id: string, childrenIds: string[], language?: string): Promise<void>;
     deleteChildrenOrder(id: string, language?: string): Promise<void>;
